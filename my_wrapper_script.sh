394db87ae10135986fe535e966dbe144f18bdfb6
Vinay <vinayapr23@gmail.com>
2024-06-05 17:32:57 +0530

    process fail

--- my_wrapper_script.sh
+++ my_wrapper_script.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
+# Start process.py in the background
 
-
-# Start pull-service.py in the foreground (assuming it should run in the foreground)
-python pull-service.py &
-PULL_SERVICE_PID=$!
-
-# Start app.py with Gunicorn in the background
 python app.py &
-APP_PID=$!
+FILE1_PID=$!
 
 
+
+python pull-service.py
 
 cleanup() {
     echo "Stopping services..."
-    kill  $APP_PID $PULL_SERVICE_PID
+    kill $FILE1_PID  # Kill both processes
     exit
 }
+
 
 # Trap termination signals
 trap cleanup INT TERM
 
-# Monitor all processes
+
 while true; do
-    if ! kill -0 $APP_PID >/dev/null 2>&1; then
-        echo "app.py has exited."
-        exit 1
-    fi
-    if ! kill -0 $PULL_SERVICE_PID >/dev/null 2>&1; then
-        echo "pull-service.py has exited."
+    if ! kill -0 $FILE2_PID >/dev/null 2>&1; then
+        echo "process.py has exited."
         exit 1
     fi
     sleep 1
